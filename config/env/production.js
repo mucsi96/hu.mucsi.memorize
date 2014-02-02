41c3d64b6ee054a91d562d54ad778933b3cf79f7
Igor <mucsi_96@yahoo.com>
2014-02-02 20:46:01 +0100

    Production google callback updated

--- config/env/production.js
+++ config/env/production.js
@@ -5,6 +5,6 @@ module.exports = {
         name: "MEAN - A Modern Stack - Production"
     },
     google: {
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "http://hu-mucsi-memorize.herokuapp.com/auth/google/callback"
     }
 }
