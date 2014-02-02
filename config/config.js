6198cb19163bb2e6a5d243038b1ff57e83ac3542
Igor <mucsi_96@yahoo.com>
2014-02-02 20:32:05 +0100

    Simplify configurations

--- config/config.js
+++ config/config.js
@@ -2,10 +2,22 @@
 
 // Utilize Lo-Dash utility library
 var _ = require('lodash');
+var envConf = require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
+var path = require('path');
+var rootPath = path.normalize(__dirname + '/..');
 
-// Extend the base configuration in all.js with environment
-// specific configuration
-module.exports = _.extend(
-    require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
-);
+module.exports = _.merge({
+    root: rootPath,
+    port: process.env.PORT || 3000,
+    db: process.env.MONGOHQ_URL,
+    google: {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+
+    // The secret should be set to a non-guessable string that
+    // is used to compute a session hash
+    sessionSecret: 'MEAN',
+    // The name of the MongoDB collection to store sessions in
+    sessionCollection: 'sessions'
+},envConf);
