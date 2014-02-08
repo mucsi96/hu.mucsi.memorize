e8bec4286bef6ecf098c68e1018e81ce4acfb4fc
Igor <mucsi_96@yahoo.com>
2014-02-08 14:50:23 +0100

    Init i18

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.wordsets']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'pascalprecht.translate', 'mean.system', 'mean.articles', 'mean.wordsets']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
---
+++ public/js/routeConfig.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Setting up route
+angular.module('mean').config(['$routeProvider', function($routeProvider) {
+    if (window.user) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/wordsets.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    } else {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/index.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }
+}]);
+
+//Setting HTML5 Location Mode
+angular.module('mean').config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
+]);
