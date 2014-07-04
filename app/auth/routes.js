21a315bbbad5fad2cae695ec3a48887c29899927
Igor Mucsicska <mucsi_96@yahoo.com>
2014-07-04 23:02:07 +0200

    Auth API

--- app/auth/routes.js
+++ app/auth/routes.js
@@ -1,22 +1,21 @@
 'use strict';
 
 // User routes use users controller
-var controller = require('./controller');
+var auth = require('./controller');
 
 module.exports = function(app, passport) {
 
-    app.get('/signout', controller.signout);
+    app.get('/auth', auth.status);
+
+    app.get('/auth/signout', auth.signout);
 
     app.get('/auth/google', passport.authenticate('google', {
-        failureRedirect: '/signin',
         scope: [
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email'
         ]
     }));
 
-    app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: '/signin'
-    }), controller.authCallback);
+    app.get('/auth/google/callback', passport.authenticate('google'), auth.authCallback);
 
 };
--- app/wordsets/routes.js
+++ app/wordsets/routes.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var controller = require('./controller'),
-    authorization = require('../users/controller');
+    auth = require('../auth/controller');
 
 module.exports = function (app) {
-    app.get('/wordsets', authorization.requiresLogin, controller.all);
-    app.post('/wordsets', authorization.requiresLogin, controller.create);
-    app.del('/wordsets/:wordsetId', authorization.requiresLogin, controller.destroy);
-    app.put('/wordsets/:wordsetId', authorization.requiresLogin, controller.update);
+    app.get('/wordsets', auth.requiresLogin, controller.all);
+    app.post('/wordsets', auth.requiresLogin, controller.create);
+    app.del('/wordsets/:wordsetId', auth.requiresLogin, controller.destroy);
+    app.put('/wordsets/:wordsetId', auth.requiresLogin, controller.update);
 
     app.param('wordsetId', controller.wordset);
 };
